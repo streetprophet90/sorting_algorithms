c909cc79c28550e7379e480d0f32fe9146f7c831
streetprophet90 <hamoafo@gmail.com>
2023-04-28 04:44:00 -0700

    Sorting tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,11 +7,9 @@
  */
 void swap_ints(int *a, int *b)
 {
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 /**
@@ -23,24 +21,24 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+    if (array == NULL || size < 2)
+        return;
 
-	if (array == NULL || size < 2)
-		return;
+    size_t len = size;
+    bool bubbly = false;
 
-	while (bubbly == false)
-	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_ints(array + i, array + i + 1);
-				print_array(array, size);
-				bubbly = false;
-			}
-		}
-		len--;
-	}
-}}
+    while (!bubbly)
+    {
+        bubbly = true;
+        for (size_t i = 0; i < len - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap_ints(&array[i], &array[i + 1]);
+                print_array(array, size);
+                bubbly = false;
+            }
+        }
+        len--;
+    }
+}
